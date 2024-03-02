fb312bdd4885fc2c8e832acaad335d688cfdf245
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-03-02 15:38:51 +0530

    02-March-2024

---
+++ March2024/02-march-prblm/firstElementToOccurKTimes.java
@@ -0,0 +1,61 @@
+package 02-march-prblm;
+
+public class firstElementToOccurKTimes {
+
+    Name Of The Problem :-First element to occur k times
+
+    POTD :- 02-march-2024
+
+
+    Given an array of n integers. Find the first element that occurs atleast k number of times.
+
+Example 1:
+
+Input :
+n = 7, k = 2
+a[] = {1, 7, 4, 3, 4, 8, 7}
+Output :
+4
+Explanation :
+Both 7 and 4 occur 2 times. But 4 is first that occurs twice.
+As at index = 4, 4 has occurred twice whereas 7 appeared twice
+at index 6.
+Example 2:
+
+Input :
+n = 10, k = 3
+a[] = {3, 1, 3, 4, 5, 1, 3, 3, 5, 4}
+Output :
+3
+Explanation :
+Here, 3 is the only number that appeared atleast 3 times in the array.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function firstElementKTime() which takes the array a[], its size n, and an integer k as input arguments and returns the required answer. If the answer is not present in the array, return -1.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+
+Constraints:
+1 <= n <= 104
+1 <= k <= 100
+0<= a[i] <= 200
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Solution
+{
+    public int firstElementKTime(int n, int k, int[] a) { 
+        
+        HashMap<Integer,Integer> mm = new HashMap<>();
+        for(int i=0;i<n;i++){
+            mm.putIfAbsent(a[i],0);
+            mm.put(a[i],mm.get(a[i])+1);
+            if(mm.get(a[i])==k)return a[i];
+        }
+        return -1;
+        
+    } 
+}
+    
+}
