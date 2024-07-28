46799c44dfbbe19247322cbce5f5d863312f32fc
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-07-28 12:40:45 +0530

    28-July-2024

---
+++ July2024/28-jul-prblm/removeDuplicates.java
@@ -0,0 +1,51 @@
+package 28-jul-prblm;
+
+public class removeDuplicates {
+
+
+
+    Name Of The Problem :- Remove Duplicates
+
+
+    POTD :- 28-July-2024
+
+    Given a string str without spaces, the task is to remove all duplicate characters from it, keeping only the first occurrence.
+
+Note: The original order of characters must be kept the same. 
+
+Examples :
+
+Input: str = "zvvo"
+Output: "zvo"
+Explanation: Only keep the first occurrence
+Input: str = "gfg"
+Output: "gf"
+Explanation: Only keep the first occurrence
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= |str| <= 105
+str contains lowercase English alphabets
+
+*****************************************************************************************************************************************************
+
+
+
+class Solution {
+    String removeDups(String str) {
+        // code here
+         StringBuilder ans = new StringBuilder();
+        boolean temp[]=new boolean[26];
+        for(int i=0;i<str.length();i++){
+            char x = str.charAt(i);
+            if(!temp[x-'a']){
+                ans.append(x);
+                temp[x-'a']=true;
+            }
+        }
+        return ans.toString();
+    }
+}
+    
+}
