be115d5ccf23a405ddd974989ffa4ccaa0b45981
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-09-13 18:01:43 +0530

    13-Sept-2024

---
+++ Sept2024/13-Sept-prblm/mirrorTree.java
@@ -0,0 +1,80 @@
+package 13-Sept-prblm;
+
+public class mirrorTree {
+
+    Name Of The Problem :- Mirror Tree
+
+    POTD :- 13-September-2024
+
+
+    Given a Binary Tree, convert it into its mirror.
+MirrorTree1            
+
+Examples:
+
+Input:
+      1
+    /  \
+   2    3
+Output: 3 1 2
+Explanation: The tree is
+  1    (mirror)     1
+ /  \    =>        /  \
+2    3           3   2
+The inorder of mirror is 3 1 2
+Input:
+      10
+     /  \
+    20   30
+   /  \
+  40  60
+Output: 30 10 60 20 40
+Explanation: The tree is
+      10               10
+    /    \  (mirror)    /    \
+   20    30    =>   30    20
+  /  \                     /   \
+ 40  60                 60   40
+The inroder traversal of mirror is: 30 10 60 20 40.
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(height of the tree)
+
+Constraints:
+1 ≤ Number of nodes ≤ 105
+1 ≤ Data of a node ≤ 105
+
+*****************************************************************************************************************************************************
+
+// User function Template for Java
+
+// function Template for Java
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if(node==null)return;
+        mirror(node.left);
+        mirror(node.right);
+        Node temp = node.left;
+        node.left=node.right;
+        node.right=temp;
+    }
+}
+
+
+    
+}
