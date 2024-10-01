303035d6022553123ec9e40b56a39889b453c560
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-10-01 19:11:03 +0530

    01-Oct-2024

---
+++ Oct2024/01-Oct-prblm/multiplytwolinkedlists.java
@@ -0,0 +1,67 @@
+import java.util.LinkedList;
+
+package 01-Oct-prblm;
+
+
+public class multiplytwolinkedlists {
+
+    
+
+    Name Of The Problem :- Multiply two linked lists
+
+
+    POTD :- 01-October-2024
+
+    Given elements as nodes of the two singly linked lists. The task is to multiply these two linked lists, say L1 and L2.
+
+Note: The output could be large take modulo 109+7.
+
+Examples :
+
+Input: LinkedList L1 : 3->2 , LinkedList L2 : 2
+Output: 64
+Explanation: 
+
+Multiplication of 32 and 2 gives 64.
+Input: LinkedList L1: 1->0->0 , LinkedList L2 : 1->0
+Output: 1000
+Explanation: 
+
+Multiplication of 100 and 10 gives 1000.
+Expected Time Complexity: O(max(n,m))
+Expected Auxilliary Space: O(1)
+where n is the size of L1 and m is the size of L2
+
+Constraints:
+1 <= number of nodes <= 9
+0 <= node->data <= 9
+
+*****************************************************************************************************************************************************
+/* 
+class Node
+{
+    int data;
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class Solution {
+      public static long listToNum(Node node){
+        long num = 0,mod=1000000007;
+        while(node!=null){
+            num = (num*10+node.data)%mod;
+            node=node.next;
+        }
+        return num;
+    }
+    public long multiplyTwoLists(Node first, Node second) {
+        // Code here
+         long num1 = listToNum(first);
+        long num2 = listToNum(second);
+        return (num1*num2)%1000000007;
+    }
+}
+}
