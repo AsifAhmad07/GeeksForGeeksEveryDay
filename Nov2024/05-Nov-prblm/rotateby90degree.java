2ddf39c92b66da95aca8402544b4c3bb40635adf
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-11-05 18:13:37 +0530

    05-Nov-2024

---
+++ Nov2024/05-Nov-prblm/rotateby90degree.java
@@ -0,0 +1,55 @@
+package 05-Nov-prblm;
+
+public class rotateby90degree {
+
+
+    Name Of The Problem :- Rotate by 90 degree
+
+    POTD :- 05-Novemeber-2024
+
+    Given a square mat[][]. The task is to rotate it by 90 degrees in clockwise direction without using any extra space.
+
+Examples:
+
+Input: mat[][] = [[1 2 3], [4 5 6], [7 8 9]]
+Output:
+7 4 1 
+8 5 2
+9 6 3
+Input: mat[][] = [1 2], [3 4]
+Output:
+3 1 
+4 2
+Input: mat[][] = [[1]]
+Output:
+1
+Constraints:
+1 ≤ mat.size() ≤ 1000
+1 <= mat[][] <= 100
+
+*****************************************************************************************************************************************************
+
+
+class GFG {
+    static void rotate(int matrix[][]) {
+        // Code Here
+        
+         int n = matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
+    
+}
