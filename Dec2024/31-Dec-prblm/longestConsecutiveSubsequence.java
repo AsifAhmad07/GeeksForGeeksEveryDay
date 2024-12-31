2d3e4fc6ba726a1e0c4e25e51252bf2a6a94ccfa
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-12-31 13:59:46 +0530

    31-Dec-2024

---
+++ Dec2024/31-Dec-prblm/longestConsecutiveSubsequence.java
@@ -0,0 +1,55 @@
+package 31-Dec-prblm;
+
+public class longestConsecutiveSubsequence {
+
+
+    Name Of The Problem :- Longest Consecutive Subsequence
+
+    POTD :- 31-December-2024
+
+    Given an array arr[] of non-negative integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+
+Examples:
+
+Input: arr[] = [2, 6, 1, 9, 4, 5, 3]
+Output: 6
+Explanation: The consecutive numbers here are 1, 2, 3, 4, 5, 6. These 6 numbers form the longest consecutive subsquence.
+Input: arr[] = [1, 9, 3, 10, 4, 20, 2]
+Output: 4
+Explanation: 1, 2, 3, 4 is the longest consecutive subsequence.
+Input: arr[] = [15, 13, 12, 14, 11, 10, 9]
+Output: 7
+Explanation: The longest consecutive subsequence is 9, 10, 11, 12, 13, 14, 15, which has a length of 7.
+Constraints:
+1 <= arr.size() <= 105
+0 <= arr[i] <= 105
+
+
+
+*****************************************************************************************************************************************************
+
+
+class Solution {
+
+    // Function to return length of longest subsequence of consecutive integers.
+    public int longestConsecutive(int[] arr) {
+        // code here
+        
+         int ans=1;
+        HashMap<Integer,Integer> mm = new HashMap<>();
+        for(int x:arr){
+            if(mm.get(x)==null){
+                int left = mm.getOrDefault(x-1,0);
+                int right = mm.getOrDefault(x+1,0);
+                int total = left+right+1;
+                mm.put(x,total);
+                mm.put(x-left,total);
+                mm.put(x+right,total);
+                ans=Math.max(ans,total);
+            }
+        }
+        return ans;
+    }
+}
+    
+}
