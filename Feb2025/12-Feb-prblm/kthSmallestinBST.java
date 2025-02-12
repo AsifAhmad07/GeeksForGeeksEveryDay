83ac993837e35f89776a5ae1896e480d011a8091
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2025-02-12 14:32:38 +0530

    12-Feb-2025

---
+++ Feb2025/12-Feb-prblm/kthSmallestinBST.java
@@ -0,0 +1,76 @@
+package 12-Feb-prblm;
+
+public class kthSmallestinBST {
+
+    Name Of The problem :- k-th Smallest in BST
+
+
+    POTD :- 12-February-2025
+
+    Given a BST and an integer k, the task is to find the kth smallest element in the BST. If there is no kth smallest element present then return -1.
+
+Examples:
+
+Input: root = [2, 1, 3], k = 2
+    
+Output: 2
+Explanation: 2 is the 2nd smallest element in the BST.
+Input: root = [2, 1, 3], k = 5
+    
+Output: -1
+Explanation: There is no 5th smallest element in the BST as the size of BST is 3.
+Input: root = [20, 8, 22, 4, 12, N, N, N, N, 10, 14], k = 3
+     
+Output: 10
+Explanation: 10 is the 3rd smallest element in the BST.
+Constraints:
+
+1 <= number of nodes, k <= 105
+1 <= node->data <= 105
+
+*****************************************************************************************************************************************************
+
+
+// class Node
+// {
+//     int data;
+//     Node left, right;
+
+//     public Node(int d)
+//     {
+//         data = d;
+//         left = right = null;
+//     }
+// }
+
+class Solution {
+    int count = 0;
+    int res = -1;
+    // Return the Kth smallest element in the given BST
+    public int kthSmallest(Node root, int k) {
+        // Write your code here
+        count = k;
+        res = -1;
+        utils(root);
+        return res;
+    }
+    
+    void utils(Node root){
+        if(root == null){
+            return;
+        }
+        
+        utils(root.left);
+        --count;
+        
+        if(count == 0){
+            res = root.data;
+            return;
+        }
+        
+        utils(root.right);
+    }
+    
+}
+    
+}
