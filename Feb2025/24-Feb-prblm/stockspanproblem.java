efd145b4e7f454c23e691ff238de61c6311a80a0
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2025-02-24 20:46:22 +0530

    24-Feb-2025

---
+++ Feb2025/24-Feb-prblm/stockspanproblem.java
@@ -0,0 +1,55 @@
+package 24-Feb-prblm;
+
+public class stockspanproblem {
+
+
+    Name Of The Problem :- Stock span problem
+
+
+    POTD :- 24-February-2025
+
+    The stock span problem is a financial problem where we have a series of daily price quotes for a stock and we need to calculate the span of stock price for all days. The span arr[i] of the stocks price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on the given day is less than or equal to its price on the current day.
+
+Examples:
+
+Input: arr[] = [100, 80, 60, 70, 60, 75, 85]
+Output: [1, 1, 1, 2, 1, 4, 6]
+Explanation: Traversing the given input span 100 is greater than equal to 100 and there are no more elements behind it so the span is 1, 80 is greater than equal to 80 and smaller than 100 so the span is 1, 60 is greater than equal to 60 and smaller than 80 so the span is 1, 70 is greater than equal to 60,70 and smaller than 80 so the span is 2 and so on.  Hence the output will be 1 1 1 2 1 4 6.
+Input: arr[] = [10, 4, 5, 90, 120, 80]
+Output: [1, 1, 2, 4, 5, 1]
+Explanation: Traversing the given input span 10 is greater than equal to 10 and there are no more elements behind it so the span is 1, 4 is greater than equal to 4 and smaller than 10 so the span is 1, 5 is greater than equal to 4,5 and smaller than 10 so the span is 2,  and so on. Hence the output will be 1 1 2 4 5 1.
+Constraints:
+1 ≤ arr.size()≤ 105
+1 ≤ arr[i] ≤ 105
+
+
+
+*****************************************************************************************************************************************************
+
+
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+        
+         Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+            
+            res[i] = (st.isEmpty()) ? (i + 1) : (i - st.peek());
+            st.push(i);
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int val : res) {
+            ans.add(val);
+        }
+        return ans;
+    }
+}
+    
+}
